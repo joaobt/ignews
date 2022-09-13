189f21f399aa9bd54a7dc44c45d323d81f75a536
joao <joao4729@gmail.com>
2022-09-13 13:33:15 -0300

    Components-Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
-import styles from '../styles/home.module.scss'
+import Head from "next/head";
+
+
 
 export default function Home() {
-  return <h1 
-    className={styles.title}>
-    Hello <span>word</span> 
-  </h1>;
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>
+        Hello word
+      </h1>
+    </>
+  );
 }
