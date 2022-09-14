678dd1132e8b1e39ec075aa433e579ee9ec54ac7
joao <joao4729@gmail.com>
2022-09-14 15:52:18 -0300

    Consumindo-API-stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,60 @@
+import { GetServerSideProps } from 'next';
+
 import Head from "next/head";
+import { stripe } from '../services/stripe'
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from './home.module.scss';
 
+interface HomeProps {
+  product: {
+    preceId: string;
+    amount: number;
+  }
+}
 
+export default function Home({ product }: HomeProps) {
 
-export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-        <main className={styles.contentContainer}>
-          <section className={styles.hero}>
-           <span>👏 Hey, welcome</span>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
 
-           <h1>News about the <span>React</span>word.</h1>
-           <p>
-            Get access to all the publications <br/>
-            <span>for $9.90 month</span>
-           </p>
-          </section>
-            <img src="/images/avatar.svg" alt="Girl coing" />
-        </main>
+          <h1>News about the <span>React</span>word.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount}month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
-  );
+  )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1LhtVJJPPcNARZTQeMFHcwZU', {
+      expand: ['product']
+    })
+
+   
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
+}
+
+
